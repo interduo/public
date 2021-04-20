48746f7e984c4567d3bf767f7ca0cf243ef17cad
Interduo <jkl@interduo.pl>
2021-04-20 16:16:58 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+
+$visible1 = "";
+$visible2 = null;
+$visible3 = 0;
+$visible4 = 1;
+
+$visible1 = ($visible1=="" ? false : true);
+print 'visible1 ' . $visible1 . ' ';
+
+$visible2 = ($visible2=="" ? false : true);
+print 'visible2 ' . $visible2 . ' ';
+
+$visible3 = ($visible3=="" ? false : true);
+print 'visible3 ' . $visible3 . ' ';
+
+$visible4 = ($visible4=="" ? false : true);
+print 'visible4 ' . $visible4 . ' ';
+
+?>
