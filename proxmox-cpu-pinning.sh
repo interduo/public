b78b45f6319459f30a4f503627d7169f4d5ff552
Interduo <jkl@interduo.pl>
2022-06-10 17:27:53 +0200

    Update proxmox-cpu-pinning.sh

--- proxmox-cpu-pinning.sh
+++ proxmox-cpu-pinning.sh
@@ -4,6 +4,7 @@ PHASE=$2
 
 if [ -z $VMID ] || [ -z $PHASE ]
 then
+  echo 'Error: CPU Pining not set';
   exit 1;
 fi
 
@@ -11,11 +12,13 @@ if [ "${VMID}" -eq 900 ]
 then
   CPUSET="0-13"
 else
-  CPUSET="14-27"
+  CPUSET="0-27"
 fi
 
 if [ "${PHASE}" == "post-start" ]
 then
     MAIN_PID="$(< /run/qemu-server/${VMID}.pid)"
-    echo taskset --cpu-list --all-tasks --pid "${CPUSET}" "${MAIN_PID}"
+    taskset --cpu-list --all-tasks --pid "${CPUSET}" "${MAIN_PID}"
 fi
+
+echo 'CPU Pining set OK';
