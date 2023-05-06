a1ea3f49b4a6293b50917b9351d141679204b316
Interduo <jkl@interduo.pl>
2023-05-06 11:44:32 +0200

    Create handy-php-functions.php

---
+++ handy-php-functions.php
@@ -0,0 +1,16 @@
+<?php
+// Those functions are from stackoverflow, github and many others sources
+
+function delete_all_between($beginning, $end, $string) {
+    $beginningPos = strpos($string, $beginning);
+    $endPos = strpos($string, $end);
+    if ($beginningPos === false || $endPos === false) {
+        return $string;
+    }
+
+    $textToDelete = substr($string, $beginningPos, ($endPos + strlen($end)) - $beginningPos);
+
+    return delete_all_between($beginning, $end, str_replace($textToDelete, '', $string)); // recursion to ensure all occurrences are replaced
+}
+
+?>
