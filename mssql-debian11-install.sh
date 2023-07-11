d6d0d1bf2dd71943adbd6ef371aa6059a1920895
Interduo <jkl@interduo.pl>
2023-07-11 15:47:41 +0200

    Update mssql-debian11-install.sh

--- mssql-debian11-install.sh
+++ mssql-debian11-install.sh
@@ -6,3 +6,14 @@ curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/source
 ACCEPT_EULA=Y
 
 apt update && apt install -y msodbcsql18 mssql-tools18
+
+wget https://github.com/microsoft/msphpsql/releases/download/v5.10.1/Debian11-7.4.tar -O /tmp/d11.tar
+tar -xvf /tmp/d11.tar
+cp /tmp/Debian7.4/php_pdo_sqlsrv_74_nts.so /usr/lib/php/20210902/pdo_sqlsrv.so
+cp /tmp/Debian7.4/php_sqlsrv_74_nts.so /usr/lib/php/20210902/sqlsrv.so
+rm /tmp/d11.tar
+
+printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/7.4/mods-available/sqlsrv.ini
+printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/7.4/mods-available/pdo_sqlsrv.ini
+
+phpenmod sql_srv pdo_sqlsrvx
