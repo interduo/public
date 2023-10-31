2034c448c2aab7c9534028bc68669893fcbad127
Interduo <jkl@interduo.pl>
2023-10-31 10:24:42 +0100

    Update gammu-smsd-runonreceive-wrapper.sh

--- gammu-smsd-runonreceive-wrapper.sh
+++ gammu-smsd-runonreceive-wrapper.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 # author: Jarosław Kłopotek <jkl@interduo.pl> github:interduo
 # put the script in /etc/gammu-smsdrc in runonreceive variable, merges long SMSes and send mail with SMS content
-# requires: find, awk, msmtp,
+# requires: find, msmtp,
 
-GAMMU_DIR='/var/spool/gammu'
 GAMMU_LMSOUT='/var/spool/gammu/wyslanedolms'
 GAMMU_INBOX='/var/spool/gammu/inbox'
-RTMAIL='bok@domain.pl';
+RTMAIL='domain@your-company.pl';
 MAILCONTENTTYPE="Content-type: text/plain; charset=utf-8"
-ELEMENTSCOUNT=( $# )
 
-SMSCONTENT=`cd ${GAMMU_INBOX} && find "$@" -type f -exec cat {} \; -exec rm {} \;`
-NEWNAME=`echo $1 | tr ' ' '_'`
-NUMBER=`echo "${NEWNAME}" | awk -F '/' '{print $NF}' | awk -F '_' '{print $4}'`
-echo "${SMSCONTENT}" > "${GAMMU_LMSOUT}/${NEWNAME}"
-
-echo -e "Subject:SMS:${NUMBER}\n${MAILCONTENTTYPE}\n${SMSCONTENT}\n" | msmtp -t ${RTMAIL}
+echo -e "Subject:SMS:${SMS_1_NUMBER}\n${MAILCONTENTTYPE}\n${DECODED_1_TEXT}\n" | msmtp -t ${RTMAIL}
+find ${GAMMU_INBOX} -type f -exec mv {} ${GAMMU_LMSOUT} \;
