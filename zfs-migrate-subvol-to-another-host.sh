9cb38aba0a54b9c41365b8edd72c608fe8993306
Jarosław Kłopotek - INTERDUO <jkl@interduo.pl>
2024-08-02 11:06:53 +0200

    Update zfs-migrate-subvol-to-another-host.sh

--- zfs-migrate-subvol-to-another-host.sh
+++ zfs-migrate-subvol-to-another-host.sh
@@ -1,5 +1,16 @@
 exit;
 
-#example
+#single example
 zfs snapshot zfs1-vps1/subvol-103-disk-0@migracja
 zfs send zfs1-vps1/subvol-103-disk-0@migracja | ssh root@10.100.0.4 zfs recv zfs1-vps1/subvol-103-disk-0@migracja
+
+#mass example
+srcpool='zfs-pbs1'
+dstpool='zfs-pbs1'
+
+for i in 'vm-backup'
+do
+    zfs snap ${srcpool}/${i}@snapshot
+    zfs send ${srcpool}/${i}@snapshot | ssh root@172.20.3.14 zfs recv ${dstpool}/${i}"
+    zfs destroy ${srcpool}/${i}@snapshot
+done
