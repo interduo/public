94a11e28b3bcb0b49e09328b6f313e2a94f45ef2
Jarosław Kłopotek - INTERDUO <jkl@interduo.pl>
2024-08-07 14:32:20 +0200

    Update zfs-migrate-subvol-to-another-host.sh

--- zfs-migrate-subvol-to-another-host.sh
+++ zfs-migrate-subvol-to-another-host.sh
@@ -2,7 +2,10 @@
 # example1: replicate single pool/subvol/zvol using SSH to remote host
 zfs snapshot zfs1-vps1/subvol-103-disk-0@migracja
 zfs send zfs1-vps1/subvol-103-disk-0@migracja | ssh root@10.100.0.4 zfs recv zfs1-vps1/subvol-103-disk-0@migracja
-
+#stop maszyny wirtualnej/kontenera
+zfs snapshot zfs1-vps1/subvol-103-disk-0@migracja2
+zfs send -R -I zfs1-vps1/subvol-103-disk-0@migracja zfs1-vps1/subvol-103-disk-0@migracja2 | ssh root@10.100.0.4 zfs recv zfs1-vps1
+#podmiana storage w konfigu, start maszyny
 
 # example2: mass replication whole rpool using SSH to remote host
 
@@ -24,7 +27,9 @@ zfs send -R "$new_snap" | ssh "root@${destination_host}" zfs recv -Fdu "$destina
 # Get old snapshot name.
 old_snap=$(zfs list -H -o name -t snapshot -r "$source_pool" | grep "$source_pool"@"$snapshot_string" | tail --lines=1)
 
-# Create new recursive snapshot of the whole pool.
+# Create new recursive snapshot of the whole pool (with new timestamp).
+timestamp=`date +%Y%m%d%H%M%S`
+new_snap="$source_pool"@"$snapshot_string""$timestamp"
 zfs snapshot -r "$new_snap"
 
 # Incremental replication via SSH.
@@ -35,5 +40,6 @@ delete_from=$(zfs list -H -o name -t snapshot -r "$source_pool" | grep "$snapsho
 
 for snap in $delete_from;
 do
+  echo 'run this from console after propper replication'
   echo zfs destroy "$snap"
 done
