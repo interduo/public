db6a3a5d723ee884ebd468fa4348ad9f696f4a70
Jarosław Kłopotek - INTERDUO <jkl@interduo.pl>
2024-12-03 08:58:00 +0100

    Update journalctl-examples.sh

--- journalctl-examples.sh
+++ journalctl-examples.sh
@@ -1,5 +1,8 @@
 # show logs from 5 minutes
 journalctl -u lqosd --since="5 min ago"
 
+# show logs since yesterday
+journalctl -u lqosd --since yesterday
+
 # clean logs older than 5 minutes
 journalctl --vacuum-time=5min
